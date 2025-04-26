9e9615e4ec67e4e2c648c18fdfba24e5ab5a89cd
Tiff-Maina <tiff.maina01@gmail.com>
2025-04-26 18:57:04 +0300

    code adds the numbers in the list in the code

---
+++ SumList.py
@@ -0,0 +1,14 @@
+# Sum all elements in a list
+
+# list of numbers
+numbers = [5, 8, 12, 20, 3]
+
+# Initialize a variable to store the total sum
+total = 0
+
+# Loop through each element in the list
+for num in numbers:
+    total += num  # Add each number to total
+
+# Print the final sum
+print("Sum of all elements:", total)
