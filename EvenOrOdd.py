383e7c0d866f0a9d8900bd4a40dd954f8b9e1ab2
Tiff-Maina <tiff.maina01@gmail.com>
2025-04-26 18:55:32 +0300

    checks if number is even or odd

---
+++ EvenOrOdd.py
@@ -0,0 +1,10 @@
+# Check if a number is even or odd
+
+# Input number
+number = int(input("Enter a number: "))
+
+# Check remainder when divided by 2
+if number % 2 == 0:
+    print(number, "is Even")
+else:
+    print(number, "is Odd")
